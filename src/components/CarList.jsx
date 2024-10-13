3f44c8b5da82b0fef0b2c0c40919acbd298beed8
Oleksandr Pavlii <pavliy.av@gmail.com>
2024-10-13 18:11:20 +0200

    Add logic to car list

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,5 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store/slices/carsSlice';
+
 function CarList() {
-	return <div className="">CarList</div>;
+	const dispatch = useDispatch();
+
+	const cars = useSelector((state) => {
+		return state.cars.data;
+	});
+
+	const handleRemoveCar = (id) => {
+		dispatch(removeCar(id));
+	};
+
+	const renderedCars = cars.map((car) => {
+		return (
+			<div key={car.id} className="panel">
+				<p>
+					{car.name} - ${car.cost}
+				</p>
+				<button
+					className="button is-danger"
+					onClick={() => {
+						handleRemoveCar(car.id);
+					}}
+				>
+					Delete
+				</button>
+			</div>
+		);
+	});
+
+	return (
+		<div className="car-list">
+			{renderedCars}
+			<hr />
+		</div>
+	);
 }
 
 export default CarList;
