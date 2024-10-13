7ebdcfcd483c6e64dbd37e853ea86606d32cfbf5
Oleksandr Pavlii <pavliy.av@gmail.com>
2024-10-13 18:51:17 +0200

    Add total value calculation

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -5,7 +5,7 @@ function CarList() {
 	const dispatch = useDispatch();
 
 	const cars = useSelector(({ cars: { data, searchTerm } }) => {
-		return data.filter((car) => car.name.toLowerCase().includes(searchTerm));
+		return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
 	});
 
 	const handleRemoveCar = (id) => {
